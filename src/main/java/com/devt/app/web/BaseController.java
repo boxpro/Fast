6aa3acb9f720e93cf370789cac880ae38006669d
yangjie <java.yangjie@gmail.com>
2016-11-10 18:49:32 +0800

    调整了一些基本的布局配置

--- src/main/java/com/devt/app/web/BaseController.java
+++ src/main/java/com/devt/app/web/BaseController.java
@@ -7,8 +7,6 @@ package com.devt.app.web;
 public class BaseController {
     protected final String VIEW_CONTEXT_PATH="/view/";
     protected final String ADMIN_CONTEXT_PATH="/admin/";
-
-
     protected final String INDEX="index";
     protected final String SEARCH ="search";
 }
